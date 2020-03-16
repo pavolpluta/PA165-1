7fff6dcd433fd09ad82f4a94934073405af797a5
Pavol Pluta <pavol.pluta1@gmail.com>
2020-03-16 22:12:49 +0100

    All tasks of persistance1 seminar done.

--- eshop-persistence/src/main/java/cz/fi/muni/pa165/entity/Product.java
+++ eshop-persistence/src/main/java/cz/fi/muni/pa165/entity/Product.java
@@ -1,12 +1,76 @@
 package cz.fi.muni.pa165.entity;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.util.Objects;
 
+@Entity
+@Table(name = "ESHOP_PRODUCTS")
 public class Product {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column
+    private Color color;
+
+    @Column
+    private LocalDate addedDate;
+
+    public Product() {
+    }
+
+    public Product(Long productId) {this.id = productId;}
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public LocalDate getAddedDate() {
+        return addedDate;
+    }
+
+    public void setAddedDate(LocalDate addedDate) {
+        this.addedDate = addedDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        if (!(o instanceof Product)) return false;
+
+        Product product = (Product) o;
+        return name.equals(product.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
